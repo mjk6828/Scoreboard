b3fbdb71c921c87df936af1266da615a6474409f
seungyeon <seungyeon@DESKTOP-OI9SPFU>
2020-03-29 12:56:58 +0900

    게시판 연동

--- MVC/src/main/java/com/han/pjt/web/JsonController.java
+++ MVC/src/main/java/com/han/pjt/web/JsonController.java
@@ -1,14 +1,16 @@
 package com.han.pjt.web;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import javax.annotation.Resource;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.han.pjt.service.BoardService;
+import com.han.pjt.service.MemberService;
+import com.han.pjt.vo.BoardVO;
 import com.han.pjt.vo.MemberVO;
 
 import lombok.extern.log4j.Log4j2;
@@ -16,20 +18,31 @@ import lombok.extern.log4j.Log4j2;
 @RestController
 @Log4j2
 public class JsonController {
-/*
-	@Resource(name="MemberService")
-	private MemberService memberService;
-	*/
+	
+/*	 @Resource(name="MemberService")
+	 private MemberService memberService;*/
+	 @Resource(name="BoardService")
+	 private BoardService boardService;
+	 
+	 
 	@RequestMapping("/search.do")
-	public MemberVO search() throws Exception{
+	public MemberVO search() throws Exception {
 		log.info("호출되나요");
-		
+
 		MemberVO vo = new MemberVO();
 		vo.setName("abc");
 		vo.setEmail("abc");
 		vo.setPass("abc");
-		log.info(vo.getName()+vo.getPass()+vo.getEmail());
+		log.info(vo.getName() + vo.getPass() + vo.getEmail());
 		return vo;
 	}
-	
+	@RequestMapping("/boardsearch.do")
+	public List<BoardVO> boardsearch() throws Exception{
+		
+		log.info("게시판 목록 컨트롤러");
+		List<BoardVO> boardlist = boardService.boardlist();
+			log.info(boardlist.size());
+		return boardlist;
+	}
+
 }
--- MVC/src/main/webapp/js/main.js
+++ MVC/src/main/webapp/js/main.js
@@ -5,20 +5,19 @@ function SearchFunction(){
 	console.log('검색실행');
 	
 	$.ajax({
-		url: "./search.do",
-		type: "post",
+		url: "./boardsearch.do",
+		type: "get",
 		dataType:"json",
 		contentType: "application/json",
 		success : function(data){
 		/*	var result = JSON.parse(data);*/
-			console.log('성공'+data);
-			console.log(data.name+data.pass);
-			
+			console.log(data);
 			
 			var html = "";
 			for(var j=0; j<item.length; j++){
+				var item = data[i];
 				html += '<tr>';
-				html += '<td>'+item.name+'</td><td>'+item.email+'</td>';
+				html += '<td>'+item.num+'</td><td>'+item.writer+'</td>'+'<td>'+item.subject+'</td>';
 				html +='</tr>';
 			}
 			$("#list").append(html); 
